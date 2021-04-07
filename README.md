6ef300334560e796df1d0ffab013081342044cc7
slorber <lorber.sebastien@gmail.com>
2021-04-07 11:12:05 +0200

    initial lib commit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# combine-promises
-Like Promise.all(array) but with an object instead of an array.
+# Combine-Promises
+
+Like Promise.all(array) but with an object.
+
+```ts
+import combinePromises from 'combine-promises';
+
+const { user, company } = await combinePromises({
+  user: fetchUser(),
+  company: fetchCompany(),
+});
+```
+
+Why:
+
+- Insensitive to destructuring order typos
+- Simpler async functional code
+
+Features:
+
+- TypeScript support
+- Lightweight
+- Feature complete
+- Well-tested
+- ESM / CJS
+
+## TypeScript support
+
+Good, native and strict TypeScript support:
+
+- Return type correctly inferred from the input object
+- All object values should be async
+- Only accept objects (reject arrays, null, undefined...)
+
+```ts
+const result: { user: User; company: Company } = await combinePromises({
+  user: fetchUser(),
+  company: fetchCompany(),
+});
+```
+
+## Insensitive to destructuring order
+
+A common error with `Promise.all` is to have a typo in the destructuring order.
+
+```js
+// Bad: destructuring order reversed
+const [company, user] = await Promise.all([fetchUser(), fetchCompany()]);
+```
+
+This code becomes particularly dangerous as size of the array promise grows over time.
+
+With `combinePromises`, you are using explicit names instead of array indices, which makes the code more robust and not sensitive to destructuring order:
+
+```js
+// Good: we don't care about the order anymore
+const { company, user } = await combinePromises({
+  user: fetchUser(),
+  company: fetchCompany(),
+});
+```
+
+## Simpler async functional code
+
+Suppose you have an object representing a friendship like `{user1: "userId-1", user2: "userId-2"}`, and you want to transform it to `{user1: User, user2: User}`.
+
+You can easily do that:
+
+```js
+import combinePromises from 'combine-promises';
+import { mapValues } from 'lodash';
+
+const friendsIds = { user1: 'userId-1', user2: 'userId-2' };
+
+const friends = await combinePromises(mapValues(friendsIds, fetchUserById));
+```
+
+Without this library: good luck to keep your code simple.
+
+## Inspirations
+
+Name inspired by [combineReducers](https://redux.js.org/api/combinereducers) from Redux.
---
+++ test/index.test.ts
@@ -0,0 +1,105 @@
+import combinePromises from '../src';
+
+type User = { id: number; firstName: string };
+type Company = { id: number; companyName: string };
+
+function createUser(id: number): User {
+  return { id, firstName: `user firstName ${id}` };
+}
+
+function createCompany(id: number): Company {
+  return { id, companyName: `company name ${id}` };
+}
+
+async function fetchUser(id: number): Promise<User> {
+  return createUser(id);
+}
+async function fetchCompany(id: number): Promise<Company> {
+  return createCompany(id);
+}
+
+describe('combinePromises', () => {
+  it('can handle empty object', async () => {
+    const result = await combinePromises({});
+    expect(result).toEqual({});
+  });
+
+  it('can handle 1 async value', async () => {
+    const result: { user: User } = await combinePromises({
+      user: fetchUser(1),
+    });
+    expect(result).toEqual({ user: createUser(1) });
+  });
+
+  it('can handle 2 async values of same type', async () => {
+    const result: { user: User; user2: User } = await combinePromises({
+      user: fetchUser(1),
+      user2: fetchUser(2),
+    });
+    expect(result).toEqual({ user: createUser(1), user2: createUser(2) });
+  });
+
+  it('can handle 2 async values of different types', async () => {
+    const result: { user: User; company: Company } = await combinePromises({
+      user: fetchUser(1),
+      company: fetchCompany(2),
+    });
+    expect(result).toEqual({ user: createUser(1), company: createCompany(2) });
+  });
+
+  it('can handle sync values, but TS errors', async () => {
+    // @ts-expect-error: "company" value should be async/Promise
+    const result: { user: User; company: Company } = await combinePromises({
+      user: fetchUser(1),
+      // @ts-expect-error: "company" value should be async/Promise
+      company: createCompany(2),
+    });
+    expect(result).toEqual({ user: createUser(1), company: createCompany(2) });
+  });
+
+  it('can handle arrays, but TS errors', async () => {
+    // @ts-expect-error: arrays is invalid type
+    const result: { user: User; company: Company } = await combinePromises([
+      fetchUser(1),
+      fetchCompany(2),
+    ]);
+    expect(result).toEqual({ 0: createUser(1), 1: createCompany(2) });
+  });
+
+  it('throws when illegal arg', async () => {
+    await expect(
+      // @ts-expect-error: illegal typing
+      combinePromises(undefined)
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"combinePromises does not handle argument of type undefined"`
+    );
+
+    await expect(
+      // @ts-expect-error: illegal typing
+      combinePromises(null)
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"combinePromises does not handle null argument"`
+    );
+
+    await expect(
+      // @ts-expect-error: illegal typing
+      combinePromises(1)
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"combinePromises does not handle argument of type number"`
+    );
+
+    await expect(
+      // @ts-expect-error: illegal typing
+      combinePromises('hello')
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"combinePromises does not handle argument of type string"`
+    );
+
+    await expect(
+      // @ts-expect-error: illegal typing
+      combinePromises(Symbol('hello'))
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"combinePromises does not handle argument of type symbol"`
+    );
+  });
+});
